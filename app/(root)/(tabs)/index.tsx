8a0f0f86d88890959d07f3f0906ec2b3cd7cf6d6
Jehovanie RAM <jehovanieram@gmail.com>
2025-04-28 05:50:04 +0300

    setting ui home screen

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,17 +1,55 @@
+import { Card, FeaturedCard } from "@/components/Cards";
+import Filters from "@/components/Filters";
+import Search from "@/components/Search";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
 	return (
-		<View
-			style={{
-				flex: 1,
-				justifyContent: "center",
-				alignItems: "center",
-			}}>
-			<Text className="font-bold text-3xl font-rubik-extrabold text-center my-10">
-				Welcome to the ReState
-			</Text>
-		</View>
+		<SafeAreaView className="bg-white f-full">
+			<View className="px-5">
+				<View className="flex flex-row items-center justify-between mt-5">
+					<View className="flex flex-row items-center">
+						<Image source={images.avatar} className="size-12 rounded-full" />
+						<View className="flex flex-col items-start ml-2 justify-center">
+							<Text className="text-xs font-rubik">Good Morning</Text>
+							<Text className="text-base font-rubik-medium text-black-300">Jehovanie</Text>
+						</View>
+					</View>
+					<Image source={icons.bell} className="size-6" />
+				</View>
+				<Search />
+				<View className="my-5">
+					<View className="flex flex-row items-center justify-between">
+						<Text className="text-xl font-rubik-bold text-black-300">Featured</Text>
+						<TouchableOpacity>
+							<Text className="text-base font-rubik-bold text-primary-300">See all</Text>
+						</TouchableOpacity>
+					</View>
+
+					<View className="flex flex-row gap-5 mt-5">
+						<FeaturedCard />
+						<FeaturedCard />
+					</View>
+				</View>
+
+				<View className="flex flex-row items-center justify-between">
+					<Text className="text-xl font-rubik-bold text-black-300">Our Recomendation</Text>
+					<TouchableOpacity>
+						<Text className="text-base font-rubik-bold text-primary-300">See all</Text>
+					</TouchableOpacity>
+				</View>
+
+				<Filters />
+
+				<View className="flex flex-row gap-5">
+					<Card />
+					<Card />
+				</View>
+			</View>
+		</SafeAreaView>
 	);
 }
